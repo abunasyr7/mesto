e0fa036b58ffc1e88f2c14bdc7aae29c76f0d23c
abunasyr7 <abunasyr7@yandex.kz>
2021-03-24 19:39:16 +0600

    fix: исправил наименования БЭМ в html

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let profileEditButton = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close');
 let profileInfoName = document.querySelector('.profile__info-name');
 let profileInfoText = document.querySelector('.profile__info-text');
-let popupName = document.querySelector('.popup__name');
-let popupDescription = document.querySelector('.popup__description');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupJob = document.querySelector('.popup__input_type_job');
 let popupSave = document.querySelector('.popup__save');
 
 profileEditButton.addEventListener('click', openPopup);
@@ -13,7 +13,7 @@ popupClose.addEventListener('click', closePopup);
 function openPopup(e) {
     popup.classList.add('popup_open');
     popupName.value = profileInfoName.textContent;
-    popupDescription.value = profileInfoText.textContent;
+    popupJob.value = profileInfoText.textContent;
 }
 
 function closePopup(e) {
@@ -21,8 +21,9 @@ function closePopup(e) {
 }
 
 function saveClosePopup (e) {
+    evt.preventDefault();
     profileInfoName.textContent = popupName.value;
-    profileInfoText.textContent = popupDescription.value;
+    profileInfoText.textContent = popupJob.value;
     closePopup(e);
 }
 
